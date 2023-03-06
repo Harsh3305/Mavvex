c137e1550599c08591d4fc8fd63d181bca80d13f
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-06 19:53:46 +0530

    Fixed fug

--- src/pages/usecase/EnablingData.js
+++ src/pages/usecase/EnablingData.js
@@ -19,7 +19,7 @@ export default function Comp() {
                     </div>
                     <div className={styles.cardContent}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        the industry standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
@@ -29,7 +29,7 @@ export default function Comp() {
                     </div>
                     <div className={styles.cardContent}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        the industry standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
@@ -39,7 +39,7 @@ export default function Comp() {
                     </div>
                     <div className={styles.cardContent}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        the industry standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
@@ -49,7 +49,7 @@ export default function Comp() {
                     </div>
                     <div className={styles.cardContent}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        the industry standard dummy text ever since the 1500s, when an unknown printer took a galley
                         of type and scrambled it to make a type specimen book.
                     </div>
                 </div>
