2c824639ff597d5d38e03d71e09a6bda02b5ce7b
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-06 21:42:08 +0530

    Added components for industries pages

---
+++ src/components/industryComponents/UseCases.js
@@ -0,0 +1,13 @@
+import styles from "@/styles/industryComponent/UseCases.module.css"
+export default function UseCases() {
+    return <div className={styles.main}>
+        <div >
+            Use Cases
+        </div>
+        <div>
+            <div>
+                •
+            </div>
+        </div>
+    </div>
+}
