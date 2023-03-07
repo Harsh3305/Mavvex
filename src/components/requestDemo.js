67ec594c1ee9cf9d60a8ccfaf8be93bbc7ab061f
souravb786 <souravbaghel786@gmail.com>
2023-03-07 18:52:33 +0530

    Home Page created for laptop view

--- src/components/requestDemo.js
+++ src/components/requestDemo.js
@@ -3,7 +3,7 @@ import styles from "@/css/requestDemo.module.css";
 export default function Demo() {
     return <div className={styles.main}>
         <div className={styles.thunder_icon} />
-        <div className={styles.title}>Request a personal demo</div>
+        <h1 className={styles.title}>Request a personal demo</h1>
         <div className={styles.inputForm}>
             <input
                 className={styles.emailForm}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@
 import { Inter } from 'next/font/google'
 import Title from "@/components/Title";
 import Demo from "@/components/requestDemo";
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
