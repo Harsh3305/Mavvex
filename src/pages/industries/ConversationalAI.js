dd411fe786dd756081798a96b2c6a6478872e2bb
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-10 09:18:39 +0530

    Typo fixd

--- src/pages/industries/ConversationalAI.js
+++ src/pages/industries/ConversationalAI.js
@@ -20,7 +20,7 @@ export default function ConversationalAI() {
     return <div className={styles.main}>
         <div className={styles.container}>
             <MainContent
-                industryType={"Conventional Ai"}
+                industryType={"Conversational Ai"}
                 content={"Artificial intelligence (AI) is transforming the healthcare industry by enabling faster," +
                     " more accurate diagnosis, and treatment of diseases. AI-powered tools such as machine learning" +
                     " algorithms and natural language processing can analyze vast amounts of medical data, including" +
