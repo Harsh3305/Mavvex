925fc4456fc2fab480fbaa67a4020af95a49921e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-18 11:00:53 +0530

    contact-us page left-section is remaining (#18)
    * contact-us page left-section is remaining
    
    * contact-us page completed
    
    ---------
    
    Co-authored-by: souravb786 <souravbaghel786@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import styles from "@/styles/App.module.css"
 import "@/css/title.css";
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import "@/css/contactus.css"
 
 export default function App({ Component, pageProps }) {
   return <div>
