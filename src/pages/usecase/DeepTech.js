a8d3613584862876ddff7148c23663507a4dc30e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-20 07:41:53 +0530

    Added Main component in deep tech page

--- src/pages/usecase/DeepTech.js
+++ src/pages/usecase/DeepTech.js
@@ -1,3 +1,16 @@
+import MainContent from "@/components/industryComponents/MainContent";
+import loadData from "@/dao/loadData";
+import styles from "@/styles/Industries.module.css"
 export default function Comp() {
+    return <div className={styles.main}>
+        <div className={styles.container}>
+            <MainContent
+                name={"Use Cases"}
+                industryType={"Deep Tech Product Development"}
+                content={loadData("useCasesContent")}
+                imageUrl={"/image_assets/usecases_image/block.svg"}
+            />
+        </div>
 
+    </div>
 }
