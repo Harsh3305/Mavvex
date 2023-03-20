fd20e1923d542fac0fd213eaace0a18aa3700400
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-20 08:23:22 +0530

    Enabling data page completed

--- src/pages/usecase/EnablingData.js
+++ src/pages/usecase/EnablingData.js
@@ -1,7 +1,18 @@
-import styles from "@/styles/EnablingData.module.css"
-
-export default function Comp() {
-    return <div>
+import MainContent from "@/components/industryComponents/MainContent";
+import loadData from "@/dao/loadData";
+import styles from "@/styles/Industries.module.css"
+import Works from "@/components/usecase/works";
+export default function EnablingData() {
+    return <div className={styles.main}>
+        <div className={styles.container}>
+            <MainContent
+                name={"Use Cases"}
+                industryType={"Enabling Data Privacy in Machine Learning"}
+                content={loadData("useCasesContent")}
+                imageUrl={"/image_assets/usecases_image/language.svg"}
+            />
+            <Works/>
+        </div>
 
     </div>
 }
