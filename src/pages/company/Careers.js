d93a4ed7ee94444c9040a2507a125e2fb0d38768
Harsh3305 <harshverma3305@gmail.com>
2023-03-26 15:35:45 +0530

    Completed image section on career page

--- src/pages/company/Careers.js
+++ src/pages/company/Careers.js
@@ -1,10 +1,19 @@
-import Careersbody from "@/components/Careersbody"
+import styles from "@/styles/Carrers.module.css";
+import Image from "next/image";
 
 export default function Careers() {
     return (
-        <div>
-            <Careersbody></Careersbody>
-            
+        <div className={styles.main}>
+            <div className={styles.topContainer}>
+                <Image
+                    className="contact_image_div"
+                    src={"/image_assets/background_image.jpg"}
+                    width={200} height={200} alt="back-img"
+                />
+                <div className={styles.title}>
+                    Careers
+                </div>
+            </div>
 
         </div>
     )
