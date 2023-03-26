f0e853fd88ea74ca18944ec1af1e336becb648ef
Harsh3305 <harshverma3305@gmail.com>
2023-03-26 16:21:35 +0530

    Completed top section

--- src/pages/company/Careers.js
+++ src/pages/company/Careers.js
@@ -1,5 +1,6 @@
 import styles from "@/styles/Carrers.module.css";
 import Image from "next/image";
+import loadData from "@/dao/loadData";
 
 export default function Careers() {
     return (
@@ -14,7 +15,31 @@ export default function Careers() {
                     Careers
                 </div>
             </div>
+            <div className={styles.middleContainer}>
+                <div className={styles.leftContainer}>
+                    <div className={styles.content}>
+                        <div className={styles.heading}>
+                            Join out Team
+                        </div>
+                        <div className={styles.mainContent}>
+                            {loadData("careerTeam")}
+                        </div>
+                        <div className={styles.joinNow}>
+                            Are you ready to grow with us?
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.rightContainer}>
+                    <Image
+                        src="/image_assets/Rectangle_48.png"
+                        width={500}
+                        height={500}
+                    />
+                </div>
+            </div>
+            <div className={styles.underline}>
 
+            </div>
         </div>
     )
 }
