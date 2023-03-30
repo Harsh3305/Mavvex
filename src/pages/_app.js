8bbf21dffae56af7122a40dc800984fcb8ac693e
Harsh3305 <harshverma3305@gmail.com>
2023-03-30 21:43:28 +0530

    Added tosst

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,11 +7,14 @@ import "@/css/title.css";
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import "@/css/contactus.css"
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }) {
   return <div>
     <Header/>
     <Component {...pageProps} />
+    <ToastContainer />
     <Footer/>
   </div> 
     
