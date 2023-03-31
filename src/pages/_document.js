d1c2a69524259794e5111e12090bd97926e5f984
Harsh3305 <harshverma3305@gmail.com>
2023-03-31 19:25:20 +0530

    Added title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title={"Mavvex"}>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <title>Mavvex</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
