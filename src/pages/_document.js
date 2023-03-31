e9350302eb682094394ab550f52d504c6e3d7355
Harsh3305 <harshverma3305@gmail.com>
2023-03-31 19:26:35 +0530

    REmove line

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head title={"Mavvex"}>
-          <link rel="shortcut icon" href="/favicon.ico" />
           <title>Mavvex</title>
       </Head>
       <body>
